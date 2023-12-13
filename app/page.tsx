2324fdc58cbd0eb612550f500b079750cb8697a3
Royweru <royezi17@gmail.com>
2023-12-13 13:07:53 +0300

    modified:   app/globals.css 	modified:   app/layout.tsx 	modified:   app/page.tsx 	new file:   public/images/bg1.jpeg

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import Image from 'next/image'
 import { Navbar } from '@/components/Navbar/main-navbar'
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col  justify-between p-0 w-full">
+    <main className="flex min-h-screen flex-col  justify-between p-0 w-full ">
+      <div className=" w-full h-2/4 bg-red-100 flex flex-col justify-start items-center space-y-8">
       <Navbar />
+         The main top table thing
+      </div>
     </main>
   )
 }
