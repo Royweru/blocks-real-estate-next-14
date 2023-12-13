f56e6f50a353f5f42f7e9019fb91f0e87a05dc1b
Royweru <royezi17@gmail.com>
2023-12-13 18:48:50 +0300

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import Image from 'next/image'
-import { Navbar } from '@/components/Navbar/main-navbar'
+import Image from "next/image";
+import { Navbar } from "@/components/Navbar/main-navbar";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col  justify-between p-0 w-full ">
-      <div className=" w-full h-2/4 bg-red-100 flex flex-col justify-start items-center space-y-8">
-      <Navbar />
-         The main top table thing
+      <div className="w-full min-h-max bg-url[('/images/bg1.jpeg')] flex flex-col justify-start items-start space-y-8 bg-slate-300">
+        <Navbar />
+        The main top table thing
       </div>
     </main>
-  )
+  );
 }
