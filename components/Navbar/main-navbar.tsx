010cb76c37544b61fa227f26ad310b523ef0784e
Royweru <royezi17@gmail.com>
2023-12-13 09:42:26 +0300

    modified:   components/Navbar/main-navbar.tsx

--- components/Navbar/main-navbar.tsx
+++ components/Navbar/main-navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-   <div className=' w-full py-5  border-white mb-4'>
+   <div className=' w-full py-5 bg-white text-orange-500 mb-4'>
      This is a navbar!
    </div>
   )
