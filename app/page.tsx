d34a784b90a0d1f1b96ba75e372ddb703f40b768
Royweru <royezi17@gmail.com>
2023-12-14 11:21:19 +0300

    modified:   app/page.tsx 	modified:   components/show-case.tsx 	new file:   public/images/bg2.webp 	new file:   public/images/bg3.webp

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,21 @@ import SearchBar from "@/components/search-bar";
 import Billboard from "@/components/billboard";
 import Heading from "@/components/ui/heading";
 import ContentBox from "@/components/content-box";
+import ShowCase from "@/components/show-case";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-evenly w-full ">
+    <main className="flex min-h-screen flex-col gap-y-4 justify-evenly items-center w-full ">
       <Billboard>
         <Navbar/>
         <SearchBar />
       </Billboard>
-      <div className=" bg-white w-full rounded  flex flex-col justify-evenly items-center mb-5 py-3">
+      <div className=" bg-white w-full rounded  flex flex-col justify-evenly items-center  py-3">
           <Heading title="What are you expecting to do?" subtitle=" get mind blowing deals and crazy offers" />
           <ContentBox />
       </div>
+      <ShowCase />
+
+     
     </main>
   );
 }
--- components/show-case.tsx
+++ components/show-case.tsx
@@ -26,20 +26,27 @@ const divContent = [
 const ShowCase = () => {
   return (
     <>
-      <section className="  bg-slate-100 flex flex-col relative w-full py-10 rounded gap-y-4 ">
+      <div className="  bg-slate-100 flex flex-col  w-full py-10 
+       gap-y-5 justify-center items-center px-10 ">
         <Heading title=" Some amaizing properties"/>
-        <div className=" flex justify-evenly items-center">
-        {divContent.map((box) => (
-          <div
-            key={box.id}
-            className=" flex flex-col justify-center items-center p-3 rounded-md bg-slate-200"
-          >
-             
-            {box.description}
-          </div>
-        ))}
+        <div className=" w-full flex justify-center items-center">
+        <div className=" w-full grid grid-cols-2 md:grid-cols-4 gap-x-2 gap-y-1 mx-auto">
+           <div className="  bg-[url('/images/bg2.webp')] col-span-2 h-[500px]"/>
+            <div className=" h-[500px] md:col-span-2  border-[2px]  border-neutral-200 bg-slate-100 hover:outline-black hover:cursor-pointer outline-2 flex group  justify-center items-center">
+                <p className=" font-mono font-semibold text-sm text-neutral-600 ">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+            <div className=" h-[500px] md:col-span-2   border-[2px]  border-neutral-200 bg-slate-100 hover:outline-emerald-100 hover:cursor-pointer flex  justify-center items-center">
+                <p className=" font-mono font-semibold text-sm text-neutral-600 ">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+            <div className="  bg-[url('/images/bg3.webp')] col-span-2 h-[500px]"/>
         </div>
-      </section>
+        </div>
+        
+      </div>
     </>
   );
 };
