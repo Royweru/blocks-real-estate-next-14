2c30bbaab119a6eb999595f8b21449ef74380ec5
Royweru <royezi17@gmail.com>
2023-12-15 08:16:32 +0300

    modified:   components/Navbar/main-navbar.tsx 	modified:   components/listings/card.tsx

--- components/Navbar/main-navbar.tsx
+++ components/Navbar/main-navbar.tsx
@@ -3,7 +3,7 @@ import MiniNavbar from './mini-navbar'
 import UserAvatar from './user-avatar'
 export const Navbar = () => {
   return (
-   <div className=' flex justify-between w-full py-5 bg-white  mb-4 rounded-md  border-b-4 border-orange-300 px-1'>
+   <div className=' flex justify-between w-full py-5 bg-white  mb-4 rounded-md  border-b-4 border-orange-300 px-1 shadow-lg m-2'>
     <div className=' relative'>
         <MiniNavbar />
     </div>
--- components/listings/card.tsx
+++ components/listings/card.tsx
@@ -1,26 +1,44 @@
 import React from "react";
-
+import Image from "next/image";
+import { Heart } from "lucide-react";
 const Card = () => {
   return (
-    <div className="card  w-52 bg-base-100 shadow-xl">
-      <figure>
-        <img
-          src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-          alt="Shoes"
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">
-          Shoes!
-          <div className="badge badge-secondary">NEW</div>
-        </h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <div className="badge badge-outline">Fashion</div>
-          <div className="badge badge-outline">Products</div>
-        </div>
-      </div>
-    </div>
+   <div className=" col-span-1 cursor-pointer group bg-slate-200 rounded-xl">
+       <div className=" flex flex-col gap-y-2">
+           <div 
+            className="
+              w-full aspect-square relative overflow-hidden rounded-xl
+            "
+           >
+             <Image
+              fill
+              src={"/images/bg2.webp"}
+              alt=""
+              className="
+               h-full
+               w-full
+               bg-cover
+               bg-center
+                transition
+              "
+              />
+              <div className=" absolute w-4 h-4 right-2">
+                <Heart />
+              </div>
+           </div>
+           <div className=" font-bold font-sans text-orange-400">
+               The hill gardens
+           </div>
+           <div className=" flex justify-between items-center gap-x-2 w-full">
+              <div className=" font-bold text-sm font-mono">
+                  188.99$
+              </div>
+              <p className=" font-light text-sm font-mono">
+                 50% off
+              </p>
+           </div>
+       </div>
+   </div>
   );
 };
 
