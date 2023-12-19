40d77b3c1334edf73aa28be14cb9d20799448861
Royweru <royezi17@gmail.com>
2023-12-19 07:40:19 +0300

    modified:   components/Navbar/main-navbar.tsx 	modified:   components/Navbar/mini-navbar.tsx

--- components/Navbar/main-navbar.tsx
+++ components/Navbar/main-navbar.tsx
@@ -3,10 +3,10 @@ import MiniNavbar from './mini-navbar'
 import UserAvatar from './user-avatar'
 export const Navbar = () => {
   return (
-   <div className=' flex justify-between w-full py-3  bg-gray-800 mb-4  border-b-4  px-1 shadow-lg  items-center'>
-    <div >
+   <div className=' flex justify-between w-full  bg-gray-800 mb-4  border-b-4  px-1 shadow-lg  items-center'>
+   
     <MiniNavbar />
-    </div>
+    
    </div>
   )
 }
--- components/Navbar/mini-navbar.tsx
+++ components/Navbar/mini-navbar.tsx
@@ -57,10 +57,10 @@ const MiniNavbar = () => {
                     </svg>
                 </button>
             </div>
-            <div className="hidden md:flex justify-between items-end w-full md:w-auto md:order-1" id="mobile-menu-3">
+            <div className="hidden md:flex justify-between items-end w-full md:w-auto md:order-1" >
                 <ul className="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
                     <li>
-                        <a href="#"
+                        <a href="/"
                             className="bg-blue-700 md:bg-transparent text-white block pl-3 pr-4 py-2 md:text-blue-700 md:p-0 rounded"
                             aria-current="page">Home</a>
                     </li>
@@ -73,6 +73,25 @@ const MiniNavbar = () => {
                             className="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">Services</a>
                     </li>
                 </ul>
+            </div>
+            <div className="md:hidden" >
+              {isOpen &&(
+                   <ul className="flex-col absolute  bg-slate-400 p-4 md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
+                   <li>
+                       <a href="/"
+                           className="bg-blue-700 md:bg-transparent text-white block pl-3 pr-4 py-2 md:text-blue-700 md:p-0 rounded"
+                           aria-current="page">Home</a>
+                   </li>
+                   <li>
+                       <a href="#"
+                           className="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">About</a>
+                   </li>
+                   <li>
+                       <a href="#"
+                           className="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">Services</a>
+                   </li>
+               </ul>
+              )}
             </div>
         </div>
     </div>
