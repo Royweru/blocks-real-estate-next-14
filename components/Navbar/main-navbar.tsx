b991ceece6a3aad8765d3bc0c644d463df17cd5b
Royweru <royezi17@gmail.com>
2023-12-19 09:22:49 +0300

    modified:   components/Navbar/main-navbar.tsx 	modified:   components/Navbar/mini-navbar.tsx

--- components/Navbar/main-navbar.tsx
+++ components/Navbar/main-navbar.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import MiniNavbar from './mini-navbar'
-import UserAvatar from './user-avatar'
+import React from "react";
+import MiniNavbar from "./mini-navbar";
+import UserAvatar from "./user-avatar";
 export const Navbar = () => {
   return (
-   <div className=' flex justify-between w-full  bg-gray-800 mb-4  border-b-4  px-1 shadow-lg  items-center'>
-   
-    <MiniNavbar />
-    
-   </div>
-  )
-}
+    <div className=" flex justify-between w-full  bg-gray-800 mb-4  border-b-4  px-1 shadow-lg  items-center">
+      <MiniNavbar />
+      <UserAvatar />
+    </div>
+  );
+};
