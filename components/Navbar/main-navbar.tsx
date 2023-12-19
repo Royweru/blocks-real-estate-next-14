f5bc8b28169509cb2ad5598e26e41dade40902e0
Royweru <royezi17@gmail.com>
2023-12-19 07:29:47 +0300

    modified:   components/Navbar/main-navbar.tsx 	modified:   components/Navbar/mini-navbar.tsx

--- components/Navbar/main-navbar.tsx
+++ components/Navbar/main-navbar.tsx
@@ -3,11 +3,10 @@ import MiniNavbar from './mini-navbar'
 import UserAvatar from './user-avatar'
 export const Navbar = () => {
   return (
-   <div className=' flex justify-between w-full py-3  bg-gray-800 mb-4  border-b-4  px-1 shadow-lg m-2 items-center'>
-    <div className=' relative'>
-        <MiniNavbar />
+   <div className=' flex justify-between w-full py-3  bg-gray-800 mb-4  border-b-4  px-1 shadow-lg  items-center'>
+    <div >
+    <MiniNavbar />
     </div>
-  
    </div>
   )
 }
