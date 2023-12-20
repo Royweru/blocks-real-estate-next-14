f0e0bc1ca4d52d3535e2e13d093ad4578a1f8af5
Royweru <royezi17@gmail.com>
2023-12-20 22:40:38 +0300

    modified:   components/blogs-section.tsx

--- components/blogs-section.tsx
+++ components/blogs-section.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import Image from 'next/image'
 
 const BlogsSection = () => {
   return (
@@ -9,11 +10,14 @@ const BlogsSection = () => {
           <div className="flex flex-wrap -m-4">
             <div className="p-4 md:w-1/3">
               <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                <img
-                  className="lg:h-48 md:h-36 w-full object-cover object-center"
-                  src="https://dummyimage.com/720x400"
-                  alt="blog"
-                />
+               <div className=" lg:h-48 md:h-36 w-full relative">
+                <Image
+                 fill
+                 src={"/images/bg1.jpeg"}
+                 alt=""
+                 className=" bg-cover bg-center"
+                 />
+               </div>
                 <div className="p-6">
                   <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
                     CATEGORY
@@ -76,23 +80,26 @@ const BlogsSection = () => {
             </div>
             <div className="p-4 md:w-1/3">
               <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                <img
-                  className="lg:h-48 md:h-36 w-full object-cover object-center"
-                  src="https://dummyimage.com/721x401"
-                  alt="blog"
-                />
+               <div className=" lg:h-48 md:h-36 w-full relative">
+                <Image
+                 fill
+                 src={"/images/bg1.jpeg"}
+                 alt=""
+                 className=" bg-cover bg-center"
+                 />
+               </div>
                 <div className="p-6">
                   <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
                     CATEGORY
                   </h2>
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                    The 400 Blows
+                    The Catalyzer
                   </h1>
                   <p className="leading-relaxed mb-3">
                     Photo booth fam kinfolk cold-pressed sriracha leggings
                     jianbing microdosing tousled waistcoat.
                   </p>
-                  <div className="flex items-center flex-wrap">
+                  <div className="flex items-center flex-wrap ">
                     <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
                       Learn More
                       <svg
@@ -143,17 +150,20 @@ const BlogsSection = () => {
             </div>
             <div className="p-4 md:w-1/3">
               <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                <img
-                  className="lg:h-48 md:h-36 w-full object-cover object-center"
-                  src="https://dummyimage.com/722x402"
-                  alt="blog"
-                />
+               <div className=" lg:h-48 md:h-36 w-full relative">
+                <Image
+                 fill
+                 src={"/images/bg1.jpeg"}
+                 alt=""
+                 className=" bg-cover bg-center"
+                 />
+               </div>
                 <div className="p-6">
                   <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
                     CATEGORY
                   </h2>
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                    Shooting Stars
+                    The Catalyzer
                   </h1>
                   <p className="leading-relaxed mb-3">
                     Photo booth fam kinfolk cold-pressed sriracha leggings
@@ -202,7 +212,7 @@ const BlogsSection = () => {
                       >
                         <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                       </svg>
-                      6 
+                      6
                     </span>
                   </div>
                 </div>
