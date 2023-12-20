07de5a9808584f19aa4c1ae81e38ecf15ec73beb
Royweru <royezi17@gmail.com>
2023-12-20 16:09:10 +0300

    modified:   components/footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='w-full bt-[3px] border mt-2 flex
      justify-center items-center py-2  font-bold text-sm text-emerald-600 p-5 '>
-      &copy; 2024 all rights reserved
+      &copy; 2024 all rights reserved and reserved
     </div>
   )
 }
