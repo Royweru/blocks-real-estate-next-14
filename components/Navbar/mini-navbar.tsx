298c6f5753d1d344b344d30bf75f0999058cbc69
Royweru <royezi17@gmail.com>
2023-12-20 18:17:59 +0300

    modified:   components/Navbar/mini-navbar.tsx 	modified:   components/Navbar/user-avatar.tsx

--- components/Navbar/mini-navbar.tsx
+++ components/Navbar/mini-navbar.tsx
@@ -1,22 +1,38 @@
-
-
-import { currentUser } from "@clerk/nextjs";
+import { UserButton, currentUser } from "@clerk/nextjs";
 import React, { useState, useCallback } from "react";
 import UserAvatar from "./user-avatar";
+import { Button } from "../ui/button";
+import { PlusCircle } from "lucide-react";
 
-
-
-const MiniNavbar =async () => {
-const user = await currentUser()
-  if(user){
-    return(
-      <UserAvatar />
-    )
+const MiniNavbar = async () => {
+  const user = await currentUser();
+  if (user) {
+    return (
+      <div className=" justify-center items-center">
+        <Button
+          variant="destructive"
+          className=" flex  justify-between w-full items-center"
+        >
+          <div>
+            Create listing
+          </div>
+          <PlusCircle className=" w-5 h-5" />
+        </Button>
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    );
   }
   return (
-   <div className=" relative flex justify-center items-center gap-x-4">
-       
-   </div>
+    <div className=" relative flex justify-center items-center gap-x-4">
+   <Button variant="destructive">
+     <a href="/sign-in">
+      Sign In
+     </a>
+   </Button>
+   <Button variant="outline">
+     Sign up
+   </Button>
+    </div>
   );
 };
 
