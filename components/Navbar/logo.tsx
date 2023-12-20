c3c35eec4a002b4e969524559c2519122820510e
Royweru <royezi17@gmail.com>
2023-12-20 10:42:59 +0300

    modified:   components/Navbar/logo.tsx 	modified:   components/Navbar/main-navbar.tsx 	modified:   components/Navbar/mini-navbar.tsx 	modified:   components/billboard.tsx 	new file:   public/images/logo.jpg

--- components/Navbar/logo.tsx
+++ components/Navbar/logo.tsx
@@ -1,8 +1,19 @@
+"use client"
+import Image from 'next/image'
 import React from 'react'
 
 const Logo = () => {
   return (
-    <div>This is the Logo!</div>
+   <div className=' flex justify-center items-center '>
+      <div className=' relative w-[35px] h-[35px]'>
+          <Image 
+           fill
+           className=' bg-cover bg-center rounded-full'
+           src={"/images/logo.jpg"}
+           alt=''
+           />
+      </div>
+   </div>
   )
 }
 
--- components/billboard.tsx
+++ components/billboard.tsx
@@ -8,7 +8,7 @@ const Billboard:React.FC<BillboardProps> = ({
     children
 }) => {
   return (
-    <div className=" w-full h-[500px] mb-5 bg-[url('/images/bg1.jpeg')] flex flex-col justify-between py-8 items-start space-y-8 bg-cover bg-center">
+    <div className=" w-full h-[500px] mb-5 bg-[url('/images/bg1.jpeg')] flex flex-col  justify-center py-8 items-start space-y-8 bg-cover bg-center">
        {children}
     </div>
   )
