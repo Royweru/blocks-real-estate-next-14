819df125bc5157a6d3dade3bc6f1d1ca730165b8
Royweru <royezi17@gmail.com>
2023-12-20 22:52:55 +0300

    modified:   components/Navbar/mini-navbar.tsx

--- components/Navbar/mini-navbar.tsx
+++ components/Navbar/mini-navbar.tsx
@@ -8,22 +8,22 @@ const MiniNavbar = async () => {
   const user = await currentUser();
   if (user) {
     return (
-      <div className=" justify-center items-center">
+      <div className=" flex justify-center items-center w-full relative gap-x-5 ">
         <Button
           variant="destructive"
-          className=" flex  justify-between w-full items-center"
+          className=" flex  justify-between w-2/5 items-center "
         >
           <div>
             Create listing
           </div>
           <PlusCircle className=" w-5 h-5" />
         </Button>
-        <UserButton afterSignOutUrl="/" />
+       <UserButton afterSignOutUrl=""/>
       </div>
     );
   }
   return (
-    <div className=" relative flex justify-center items-center gap-x-4">
+    <div className=" relative flex justify-center items-center gap-x-4 w-full">
    <Button variant="destructive">
      <a href="/sign-in">
       Sign In
