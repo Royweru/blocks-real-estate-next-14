e0c60cfc90fec2d352d0e5b0769b33bb4bcb4b5b
Royweru <royezi17@gmail.com>
2023-12-20 13:08:59 +0300

    modified:   components/blogs-section.tsx 	modified:   components/show-case.tsx

--- components/show-case.tsx
+++ components/show-case.tsx
@@ -30,9 +30,10 @@ const ShowCase = () => {
         <Heading title=" Some amaizing properties" />
         <div className=" w-full flex justify-center items-center">
           <div className=" w-full grid grid-cols-2 md:grid-cols-4 gap-x-2 gap-y-1 mx-auto">
-            <div className="  bg-[url('/images/bg2.webp')] col-span-2 h-[500px]" />
-            <div className=" h-[500px] col-span-2  border-[2px]  border-neutral-200 bg-slate-100 hover:outline-black hover:cursor-pointer outline-2 flex group  justify-center items-center">
-              <p className=" font-mono font-semibold text-sm text-neutral-600 ">
+            <div className="  bg-[url('/images/bg2.webp')] col-span-2 h-[500px] rounded-md" />
+            <div className=" h-[500px] col-span-2  border-[2px]  border-transparent bg-slate-100 hover:outline-black 
+            hover:cursor-pointer outline-2 flex group  justify-center items-center rounded-md">
+              <p className=" font-mono font-semibold text-sm text-neutral-600 leading-9 tracking-wide">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -42,8 +43,10 @@ const ShowCase = () => {
                 sunt in culpa qui officia deserunt mollit anim id est laborum.
               </p>
             </div>
-            <div className=" h-[500px] col-span-2   border-[2px]  border-neutral-200 bg-slate-100 hover:outline-emerald-100 hover:cursor-pointer flex  justify-center items-center">
-              <p className=" font-mono font-semibold text-sm text-neutral-600 ">
+            <div className=" h-[500px] col-span-2   border-[2px]  border-transparent 
+            bg-slate-100 hover:outline-emerald-100 
+            hover:cursor-pointer flex  justify-center rounded-md">
+              <p className=" font-mono font-semibold text-sm text-neutral-600 leading-9 tracking-wide">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -53,7 +56,7 @@ const ShowCase = () => {
                 sunt in culpa qui officia deserunt mollit anim id est laborum.
               </p>
             </div>
-            <div className="  bg-[url('/images/bg3.webp')] col-span-2 h-[500px]" />
+            <div className="  bg-[url('/images/bg3.webp')] col-span-2 h-[500px] rounded-md" />
           </div>
         </div>
       </div>
